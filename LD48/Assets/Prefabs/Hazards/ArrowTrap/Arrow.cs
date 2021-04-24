2b153564d4df858f361ebd763185f734b19c973c
OwenFeldmann <ofeldman@unca.edu>
2021-04-24 11:17:30 -0400

    Loaded art, basic level, arrow traps

---
+++ LD48/Assets/Prefabs/Hazards/ArrowTrap/Arrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2(-5, 0);
+		Destroy(gameObject, 1.0f);
+    }
+	
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // When player collides with trigger, they take damage, and projectile disappears
+        if(collision.gameObject.tag == "Player")
+        {
+            //TODO kill player
+			Debug.Log("Arrow hit player");
+            Destroy(this.gameObject);
+        }
+		else if(collision.gameObject.tag == "Ground")
+		{
+			Destroy(this.gameObject);
+		}
+    }
+	
+}
---
+++ LD48/Assets/Prefabs/Hazards/ArrowTrap/ArrowTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    
+    [Tooltip("Where arrows")]
+	[SerializeField] private Transform arrowSpawnPoint;
+    [Tooltip("Arrow prefab")]
+	[SerializeField] private Arrow arrow;
+	private bool canShoot = true;
+	
+	private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(canShoot)
+		{
+			Instantiate(arrow, new Vector3(arrowSpawnPoint.position.x, arrowSpawnPoint.position.y, arrowSpawnPoint.position.z), Quaternion.identity, transform);
+			canShoot = false;
+		}
+    }
+	
+}
