a783ccf788422b52af8bac08bca3b4c1bdaabb37
OwenFeldmann <ofeldman@unca.edu>
2021-04-25 11:26:37 -0400

    Fast fall and player in air animation added

--- LD48/Assets/Prefabs/Player/Player.cs
+++ LD48/Assets/Prefabs/Player/Player.cs
@@ -8,10 +8,11 @@ public class Player : MonoBehaviour
 	
 	[SerializeField] LayerMask groundMask;
 	
-	private enum State {IDLE, WALKING};
+	private enum State {IDLE, WALKING, INAIR};
 	
 	private float speed = 7f;
 	private float jumpForce = 8f;
+	private float initialGravityScale;
 	private Rigidbody2D rb;
 	private Animator anim;
 	
@@ -33,7 +34,9 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+		initialGravityScale = rb.gravityScale;
 		anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -51,28 +54,38 @@ public class Player : MonoBehaviour
 	
 	private void DoControls()
 	{
-		if(Input.GetKeyDown(KeyCode.Space) && OnGround())
+		bool onGround = OnGround();
+		
+		if(Input.GetKeyDown(KeyCode.Space) && onGround)
 		{//jump
 			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 		}
 		
+		if(!onGround && Input.GetKey(KeyCode.S))
+		{//fast fall
+			rb.gravityScale = 3f;
+		}
+		else
+		{
+			rb.gravityScale = initialGravityScale;
+		}
 		
 		float velX = 0;
-		if(Input.GetKey(KeyCode.D))//moving right
-		{
+		if(Input.GetKey(KeyCode.D))
+		{//moving right
 			velX += speed;
 		}
-		else if(Input.GetKey(KeyCode.A))//moving left
-		{
+		else if(Input.GetKey(KeyCode.A))
+		{//moving left
 			velX -= speed;
 		}
 		
 		rb.velocity = new Vector2(velX, rb.velocity.y);
-		UpdateVisuals(velX);
+		UpdateVisuals(velX, onGround);
 		
 	}
 	
-	private void UpdateVisuals(float velX)
+	private void UpdateVisuals(float velX, bool onGround)
 	{
 		if(velX < 0)
 		{//face left
@@ -83,13 +96,20 @@ public class Player : MonoBehaviour
 			transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
 		}
 		
-		if(Mathf.Abs(velX) > 0)
+		if(!onGround)
 		{
-			anim.SetInteger("state", (int)State.WALKING);
+			anim.SetInteger("state", (int)State.INAIR);
 		}
 		else
 		{
-			anim.SetInteger("state", (int)State.IDLE);
+			if(Mathf.Abs(velX) > 0)
+			{
+				anim.SetInteger("state", (int)State.WALKING);
+			}
+			else
+			{
+				anim.SetInteger("state", (int)State.IDLE);
+			}
 		}
 	}
 	
